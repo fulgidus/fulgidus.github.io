bc9da9ccf2349c10369c7beb3f0b9a85442d3bc8
Alessio Corsi <alessio.corsi@gmail.com>
2024-11-22 14:36:31 +0100

    Trying to optimize CSS loading

--- astro.config.ts
+++ astro.config.ts
@@ -5,26 +5,27 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://fulgidus.github.io/fulgidus', // This is your full site URL
-  base: '', //'/fulgidus', // This ensures it works from the root and not a subpath
-  server: {
-    port: 1987,
-  },
-  integrations: [
-    mdx(),
-    vue(),
-    sitemap(),
-    UnoCSS({
-      injectReset: true,
-    }),
-  ],
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: 'github-light-default',
-        dark: 'github-dark-default',
-      },
-      wrap: true,
+    site: 'https://fulgidus.github.io/fulgidus', // This is your full site URL
+    base: '', //'/fulgidus', // This ensures it works from the root and not a subpath
+    server: {
+        port: 1987,
     },
-  },
+    integrations: [
+        mdx(),
+        vue(),
+        sitemap(),
+        UnoCSS({
+            injectReset: true,
+            envMode: import.meta.env.PROD ? 'build' : 'dev'
+        }),
+    ],
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'github-light-default',
+                dark: 'github-dark-default',
+            },
+            wrap: true,
+        },
+    },
 })
