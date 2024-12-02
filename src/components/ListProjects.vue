ba301105745bb5e2be384ef83085442b4186c8b0
Alessio Corsi <alessio.corsi@gmail.com>
2024-12-02 16:01:57 +0100

    m

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -20,7 +20,7 @@ defineProps<{
     <li v-for="project in list" :key="project.text" container-link w-full flex items-center rd-2>
       <a flex items-center target="_blank" :href="project.href" :aria-label="project.text">
         <div ml-2 mr-4 pt-2>
-          <i text-4xl inline-block :class="project.icon || 'i-carbon-unknown'" />
+          <i text-4xl inline-block :class="project.icon || 'i-carbon-campsite'" />
         </div>
         <div font-normal lh-tight>
           <div text-lg hover:text-main>{{ project.text }}</div>
